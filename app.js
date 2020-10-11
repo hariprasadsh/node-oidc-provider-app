a255a7ed0427a4b7a3d1a665aada02aa549204e8
hariprasadsh <63358097+hariprasadsh@users.noreply.github.com>
2020-10-11 15:52:50 +0530

    added the oidc provider configuration

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
-const express = require("express");
-const Provider = require("oidc-provider");
-
-const app = express();
+const { Provider } = require("oidc-provider");
 
 const port = 3000;
 
-const oidc = new Provider("http://localhost:3000");
+// set the basic configuratrion
+const configuration = {
+  features: {
+    introspection: { enabled: true },
+    revocation: { enabled: true },
+    sessionManagement: { enabled: true },
+  },
+  formats: {
+    AccessToken: "jwt",
+  },
+  clients: [
+    {
+      client_id: "test_implicit_app",
+      grant_types: ["implicit"],
+      response_types: ["id_token"],
+      redirect_uris: ["https://testapp/signin-oidc"],
+      token_endpoint_auth_method: "none",
+    },
+  ],
+  claims: {
+    email: ["email", "email_verified"],
+    phone: ["phone_number", "phone_number_verified"],
+    profile: [
+      "birthdate",
+      "family_name",
+      "gender",
+      "given_name",
+      "locale",
+      "middle_name",
+      "name",
+      "nickname",
+      "picture",
+      "preferred_username",
+      "profile",
+      "updated_at",
+      "website",
+      "zoneinfo",
+    ],
+  },
+  scopes: ["api1"],
+  async findById(ctx, id) {
+    return {
+      accountId: id,
+      async claims() {
+        return { sub: id };
+      },
+    };
+  },
+};
 
-app.use("/", oidc.callback);
+const oidc = new Provider(`http://localhost:${port}`, configuration);
 
-app.listen(3000, () => {
-  console.log("Server listening to port :", port);
+// express style callback for use in express applications
+oidc.callback;
+
+oidc.listen(port, () => {
+  console.log(
+    `OIDC provider is listening to port ${port}, check http://localhost:${port}/.well-known/openid-configuration`
+  );
 });
